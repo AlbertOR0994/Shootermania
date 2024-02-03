c3e3fbf786f0a1a3d8c47d5850c78226ddfe2a68
AlbertOR0994 <alberto.ocete.rosario@080formacion.alzago.es>
2024-02-03 17:00:00 +0100

    initial screen process

---
+++ src/main.js
@@ -0,0 +1,11 @@
+const start = document.getElementById('start')
+const tutorial = document.getElementById('tutorial')
+const pregame = document.getElementById('pre-game')
+
+start.addEventListener('click', () => {
+    pregame.remove()
+})
+
+tutorial.addEventListener('click', () => {
+
+})
