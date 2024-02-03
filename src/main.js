71f1e5ce0152c93b1522e48b4243161a8590a28a
Scherezade Santana <sherezade1988@gmail.com>
2024-02-03 16:39:06 +0000

    imagen tutorial añadida

--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
 const start = document.getElementById('start')
 const tutorial = document.getElementById('tutorial')
 const pregame = document.getElementById('pre-game')
+const pTutorial = document.getElementById('tutorial-image')
+const closeTuto = document.getElementById('close-tutorial')
 
 start.addEventListener('click', () => {
     pregame.remove()
 })
 
 tutorial.addEventListener('click', () => {
+    pTutorial.style.display = 'block'
+
+})
+
+closeTuto.addEventListener('click', () => {
+    pTutorial.style.display = 'none'
 
 })
