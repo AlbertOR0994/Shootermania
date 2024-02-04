b2a8f8ad0e210e194ff17b4b6de39ae29de830f2
AlbertOR0994 <alberto.ocete.rosario@080formacion.alzago.es>
2024-02-04 19:50:30 +0100

    check

--- src/main.js
+++ src/main.js
@@ -3,9 +3,14 @@ const tutorial = document.getElementById('tutorial')
 const pregame = document.getElementById('pre-game')
 const pTutorial = document.getElementById('tutorial-image')
 const closeTuto = document.getElementById('close-tutorial')
+const mPause = document.getElementById('pause')
+const game = document.getElementById('game')
+const reset = document.getElementById('reset')
 
 start.addEventListener('click', () => {
     pregame.remove()
+    intervalPause
+    game.pause()
 })
 
 tutorial.addEventListener('click', () => {
@@ -16,4 +21,25 @@ tutorial.addEventListener('click', () => {
 closeTuto.addEventListener('click', () => {
     pTutorial.style.display = 'none'
 })
+
+const intervalPause = setInterval( () => {
+    window.addEventListener('keydown', (e) => {
+        if (e.key === "Escape") {
+            mPause.style.display = 'flex'
+            reset.addEventListener('click', () => {
+            })
+        }
+        window.addEventListener('keydown', (e) => {
+            if (e.key === "Escape") {
+                mPause.style.display = 'none'
+            }
+        })
+    })
+}, 1000)
+
+
+
+
+
+
 
