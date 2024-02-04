acaac6db1a66230c755c5b9e92b62447540f34d3
Scherezade Santana <sherezade1988@gmail.com>
2024-02-04 17:34:06 +0000

    Movimiento Sam

--- src/Character/Sam.js
+++ src/Character/Sam.js
@@ -1,33 +1,47 @@
 class Sam {
-    constructor (x, y, health, bullets){
+    constructor (x, y, parent, health, bullets){
         this.x = x
         this.y = y
         this.health = health
-        this.speed = 2
+        this.speed = 5
         this.bullets = bullets
         this.timerId = null
         this.sprite
+        this.parent = parent
+        this.directionX = 0
+        this.directionY = 0
+        this.width = 50
+        this.height = 50
+        this.isDead = false
     }
 
     insertSam(){
+        let newPlayer = document.createElement('div')
+        newPlayer.setAttribute('id', 'sam')
+        newPlayer.style.top = this.y + 'px'
+        newPlayer.style.left = this.x + 'px'
+        this.parent.appendChild(newPlayer)
+        this.sprite = newPlayer
+    }
+
+    // Movimiento horizontal
+    moveX(){  
+        let nextX = this.x + this.speed * this.directionX
+        if(nextX >= 0 && nextX <= 1390){
+          this.x += this.speed * this.directionX
+          this.sprite.style.left = this.x + 'px'
+        }
         
+      }
+    // Movimiento vertical
+    moveY(){
+        let nextY = this.y + this.speed * this.directionY
+        if(nextY >= 0 && nextY <= 190){
+          this.y += this.speed * this.directionY
+          this.sprite.style.top = this.y + 'px'
+        } 
     }
-    // Eje Y positivo es ir hacia abajo
-    moveTop(){
-
-    }
-    // Eje X positivo es ir a la derecha
-    moveRight(){
-
-    }
-    // Eje X Negativo es ir a la izquierda
-    moveLeft(){
-
-    }
-    // Eje Y negativo es ir hacia arriba
-    moveBottom(){
-
-    }
+    
     // Disparar a un enemigo
     shootEnemy(){
 
--- src/main.js
+++ src/main.js
@@ -3,9 +3,45 @@ const tutorial = document.getElementById('tutorial')
 const pregame = document.getElementById('pre-game')
 const pTutorial = document.getElementById('tutorial-image')
 const closeTuto = document.getElementById('close-tutorial')
+let board = document.getElementById('game')
+
+
 
 start.addEventListener('click', () => {
     pregame.remove()
+
+    let player = new Sam(690, 100, board, 3, 25)
+    player.insertSam()
+
+    window.addEventListener('keydown', (e) => {
+        switch(e.key){
+          case 'a':
+            player.directionX = -1
+            break
+          case 'd':
+            player.directionX = 1
+            break
+        case 'w':
+            player.directionY = -1
+            break
+        case 's':
+            player.directionY = 1
+            break
+          }
+      })
+      
+    window.addEventListener('keyup', (e) => {
+        if(e.key === 'a' || e.key === 'd' || e.key === 'w' || e.key === 's'){
+          player.directionX = 0
+          player.directionY = 0
+        }
+      })
+      let timerId = setInterval(playerMovement, 24)
+    
+    function playerMovement(){
+      player.moveX()
+      player.moveY()
+    }
 })
 
 tutorial.addEventListener('click', () => {
